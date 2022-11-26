17c101ebeed2915abada5f089de3a0602b844a03
Vova <azarsanes147@gmail.com>
2022-11-26 23:47:09 +0500

    AddApp

---
+++ WordCountLibrary/WordCountClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCountLibrary
+{
+    public class WordCountClass
+    {
+        public static int WordCount(string textString, string word)
+        {
+
+        }
+    }
+}
